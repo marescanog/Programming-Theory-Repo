b1a4293c90e5f4ef20c579a0a48657cbd72ef4c3
MarvieG <marescanog@gmail.com>
2021-07-14 19:32:38 +0800

    added title screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class LevelLoader : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+
+    }
+}
